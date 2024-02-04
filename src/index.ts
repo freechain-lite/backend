85a3d802dd0601a4db730a2d2e3eeee6d1ffe459
Udoagwa Franklin <frankudoags@gmail.com>
2024-02-04 08:26:44 +0100

    Refactor user service and add point system

--- src/index.ts
+++ src/index.ts
@@ -6,21 +6,7 @@ import errorHandler from "./utils/error.middleware";
 
 const app = express();
 
-const options: cors.CorsOptions = {
-  allowedHeaders: [
-    "Origin",
-    "X-Requested-With",
-    "Content-Type",
-    "Accept",
-    "X-Access-Token",
-  ],
-  credentials: true,
-  methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-  origin: "http://localhost:3000",
-  preflightContinue: false,
-};
-
-app.use(cors(options));
+app.use(cors());
 
 
 const port = process.env.PORT || 8000;
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,11 +1,12 @@
 import User from "../db/models/userModel";
 
 export class UserService {
-    static async createUser(address: string, points: number) {
+    static async createUser(address: string, points: number = POINT_SYSTEM.NEW_USER) {
         const user = await User.create({
             address,
             points
         });
+        return user;
     }
 
     static async checkIfUserExistsAndCreateIfUserDoesNotExist(address: string) {
@@ -13,15 +14,16 @@ export class UserService {
             address
         });
         if (!user) {
-            return await this.createUser(address, 10);
+            return await this.createUser(address);
         }
     }
 
     static async updateUser(address: string, points: number) {
         //get old points and add new points
-        const oldPoints = (await User.findOne({ address }))?.points;
+        let user = (await User.findOne({ address }));
+        if (!user) return this.createUser(address, points);
 
-        const newPoints = oldPoints! + points;
+        const newPoints = user.points + points;
 
         return await User.findOneAndUpdate({
             address
@@ -32,34 +34,42 @@ export class UserService {
 
     static async referUser(address: string, referredAddress: string) {
         this.checkIfUserExistsAndCreateIfUserDoesNotExist(referredAddress);
-        this.updateUser(address, 20);
+        this.updateUser(address, POINT_SYSTEM.REFER_USER);
     }
 
     static async tradeCreated(buyerAddress: string, sellerAddress: string) {
         this.checkIfUserExistsAndCreateIfUserDoesNotExist(buyerAddress);
         this.checkIfUserExistsAndCreateIfUserDoesNotExist(sellerAddress);
 
-        this.updateUser(buyerAddress, 20);
-        this.updateUser(sellerAddress, 20);
+        this.updateUser(buyerAddress, POINT_SYSTEM.TRADE_CREATED);
+        this.updateUser(sellerAddress, POINT_SYSTEM.TRADE_CREATED);
     }
 
     static async offerCreated(address: string, takerAddress?: string) {
         this.checkIfUserExistsAndCreateIfUserDoesNotExist(address);
-        this.updateUser(address, 5);
+        this.updateUser(address, POINT_SYSTEM.OFFER_CREATED);
 
         if (takerAddress) {
             this.checkIfUserExistsAndCreateIfUserDoesNotExist(takerAddress);
-            this.updateUser(takerAddress, 5);
+            this.updateUser(takerAddress, POINT_SYSTEM.OFFER_CREATED);
         }
     }
 
     static async bidCreated(address: string, takerAddress?: string) {
         this.checkIfUserExistsAndCreateIfUserDoesNotExist(address);
-        this.updateUser(address, 5);
+        this.updateUser(address, POINT_SYSTEM.BID_CREATED);
 
         if (takerAddress) {
             this.checkIfUserExistsAndCreateIfUserDoesNotExist(takerAddress);
-            this.updateUser(takerAddress, 5);
+            this.updateUser(takerAddress, POINT_SYSTEM.BID_CREATED);
         }
     }
+}
+
+const POINT_SYSTEM = {
+    REFER_USER: 20,
+    NEW_USER: 10,
+    TRADE_CREATED: 20,
+    OFFER_CREATED: 5,
+    BID_CREATED: 5,
 }
